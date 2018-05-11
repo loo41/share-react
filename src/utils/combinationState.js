97a3953ecb69174c0e7873a4f988a1113f703c65
听一半的歌 <1805170243Qqq.com>
2018-05-11 11:52:58 +0800

    not need

--- src/utils/combinationState.js
+++
@@ -1,6 +0,0 @@
-export default function combination(stateObject) {
-    return '?' + Object.keys(stateObject)
-                    .filter(key => !!stateObject[key])
-                    .map(key => `${key}=${encodeURIComponent(stateObject[key])}`)
-                    .join('&');
-}
